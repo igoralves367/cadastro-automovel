4acf2a58afaae2a214aa9dc3294baf2b778a8ca6
igoralves367 <igor.alves367@gmail.com>
2022-11-23 14:55:35 -0300

    Inicia o projeto

---
+++ src/main/java/br/com/tinnova/cadastroautomovel/CadastroAutomovelApplication.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.cadastroautomovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroAutomovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroAutomovelApplication.class, args);
+	}
+
+}
