7e642b392b1a3ff4fcd51ee828cafda899d40159
igoralves367 <igor.alves367@gmail.com>
2022-11-23 19:33:46 -0300

    Implementa Cliente Repository

--- src/main/java/br/com/tinnova/cadastroautomovel/veiculo/infra/VeiculoInfraRepository.java
+++ src/main/java/br/com/tinnova/cadastroautomovel/veiculo/infra/VeiculoInfraRepository.java
@@ -4,16 +4,20 @@ import org.springframework.stereotype.Repository;
 
 import br.com.tinnova.cadastroautomovel.veiculo.application.repository.VeiculoRepository;
 import br.com.tinnova.cadastroautomovel.veiculo.domain.Veiculo;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class VeiculoInfraRepository implements VeiculoRepository {
+	private final VeiculoSpringDataJPARepository veiculoSpringDataJPARepository;
 
 	@Override
 	public Veiculo salva(Veiculo veiculo) {
 		log.info("[inicia] VeiculoInfraRepository - salva");
+		veiculoSpringDataJPARepository.save(veiculo);
 		log.info("[finaliza] VeiculoInfraRepository - salva");
-		return null;
+		return veiculo;
 	}
 
 }
