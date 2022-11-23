65138adadf3c04c16372273345798cb9565f5908
igoralves367 <igor.alves367@gmail.com>
2022-11-23 15:29:39 -0300

    Configuracao na camada de Application, teste ok

--- src/main/java/br/com/tinnova/cadastroautomovel/CadastroAutomovelApplication.java
+++ src/main/java/br/com/tinnova/cadastroautomovel/CadastroAutomovelApplication.java
@@ -2,12 +2,20 @@ package br.com.tinnova.cadastroautomovel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class CadastroAutomovelApplication {
+	@GetMapping
+	public String getHomeTeste() {
+		return "Cadastro Automovel - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroAutomovelApplication.class, args);
 	}
-
 }
